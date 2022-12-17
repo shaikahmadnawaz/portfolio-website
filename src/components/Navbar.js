3632e435b5b0c82ec6de7e6980e42427cba455d6
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2022-12-18 01:04:18 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ReorderIcon from "@mui/icons-material/Reorder";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
-  return <div className="nav">Navbar</div>;
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
+  return (
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
+      <div className="toggleButton">
+        <button
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        >
+          <ReorderIcon />
+        </button>
+      </div>
+      <div className="links">
+        <Link to="/"> Home </Link>
+        <Link to="/about"> About </Link>
+        <Link to="/projects"> Projects </Link>
+        <a href="https://shaikahmadnawaz.hashnode.dev/"> Blogs </a>
+        <Link to="/contact"> Contact </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
