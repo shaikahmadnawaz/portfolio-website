cfadff9470aaec8d05bbe3eff061a7d3b82a1fc0
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2022-12-21 00:09:07 +0530

    svg added

--- src/components/home/header/Header.js
+++ src/components/home/header/Header.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./header.css";
+import Me from "../../../assets/me.svg";
 
 const Header = () => {
   return (
     <section className="header-container">
-      <div>
-        <h1>Hello, I am Nawaz</h1>
+      <div className="left">
+        <h2>Hi There!</h2>
+        <h1>I'm Nawaz</h1>
+      </div>
+      <div className="right">
+        <img src={Me} alt="Me" />
       </div>
     </section>
   );
