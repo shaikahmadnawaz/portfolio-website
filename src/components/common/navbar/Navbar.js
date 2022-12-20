550455cab9e7ad00b58c43dda9e3d1c9663760aa
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2022-12-20 14:20:20 +0530

    navbar css added

--- src/components/common/navbar/Navbar.js
+++ src/components/common/navbar/Navbar.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../navbar/navbar.css";
+import { navLinks } from "./data";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="nav-container">
+      <div className="name">Nawaz</div>
+      <nav className="nav-links">
+        <ul>
+          {navLinks.map((link) => {
+            if (link.active) {
+              return (
+                <li className="nav-active">
+                  {link?.external ? (
+                    <a href={link.href}>{link.name}</a>
+                  ) : (
+                    <Link to={link.href}>{link.name}</Link>
+                  )}
+                </li>
+              );
+            }
+          })}
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
---
+++ src/components/common/navbar/data.js
@@ -0,0 +1,28 @@
+export const navLinks = [
+  {
+    name: "Home",
+    href: "/",
+    external: false,
+    active: true,
+  },
+  {
+    name: "About",
+    href: "/about",
+    external: false,
+  },
+  {
+    name: "Projects",
+    href: "/projects",
+    external: false,
+  },
+  {
+    name: "Blogs",
+    href: "https://shaikahmadnawaz.hashnode.dev/",
+    external: true,
+  },
+  {
+    name: "Contact",
+    href: "/contact",
+    external: false,
+  },
+];
