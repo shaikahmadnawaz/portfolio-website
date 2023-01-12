25ee7ba2d80f650d90ba6f37ba01aea11cccf282
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-01-12 16:43:59 +0530

    projects css done

--- src/components/home/projects/Projects.js
+++ src/components/home/projects/Projects.js
@@ -1,27 +1,93 @@
 import React from "react";
-import ProjectImage from "../../../assets/projects.png";
+import ProjectCover from "../../../assets/port1.jpg";
 import "./projects.css";
 
 const Projects = () => {
   return (
     <div className="container">
       <h1 className="sub-title">My Work</h1>
-      <div className="project-list">
-        <div className="projects">
-          <div className="project1">
-            <img src={ProjectImage} alt="" />
+      <p class="port-text">
+        Here is some of my work that I've done using technologies.
+      </p>
+      <div className="projects">
+        <div class="project-item">
+          <div class="image">
+            <img src={ProjectCover} alt="" />
           </div>
-          <div className="project1">
-            <img src={ProjectImage} alt="" />
+          <div class="hover-items">
+            <h3>Project Name</h3>
+            <div class="icons">
+              <a href="#" class="icon">
+                <i class="fab fa-github"></i>
+              </a>
+              <a href="#" class="icon">
+                <i class="fas fa-link"></i>
+              </a>
+            </div>
           </div>
-          <div className="project1">
-            <img src={ProjectImage} alt="" />
+        </div>
+        <div class="project-item">
+          <div class="image">
+            <img src={ProjectCover} alt="" />
           </div>
-          <div className="project1">
-            <img src={ProjectImage} alt="" />
+          <div class="hover-items">
+            <h3>Project Name</h3>
+            <div class="icons">
+              <a href="#" class="icon">
+                <i class="fab fa-github"></i>
+              </a>
+              <a href="#" class="icon">
+                <i class="fas fa-link"></i>
+              </a>
+            </div>
           </div>
-          <div className="project1">
-            <img src={ProjectImage} alt="" />
+        </div>
+        <div class="project-item">
+          <div class="image">
+            <img src={ProjectCover} alt="" />
+          </div>
+          <div class="hover-items">
+            <h3>Project Name</h3>
+            <div class="icons">
+              <a href="#" class="icon">
+                <i class="fab fa-github"></i>
+              </a>
+              <a href="#" class="icon">
+                <i class="fas fa-link"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="image">
+            <img src={ProjectCover} alt="" />
+          </div>
+          <div class="hover-items">
+            <h3>Project Name</h3>
+            <div class="icons">
+              <a href="#" class="icon">
+                <i class="fab fa-github"></i>
+              </a>
+              <a href="#" class="icon">
+                <i class="fas fa-link"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="image">
+            <img src={ProjectCover} alt="" />
+          </div>
+          <div class="hover-items">
+            <h3>Project Name</h3>
+            <div class="icons">
+              <a href="#" class="icon">
+                <i class="fab fa-github"></i>
+              </a>
+              <a href="#" class="icon">
+                <i class="fas fa-link"></i>
+              </a>
+            </div>
           </div>
         </div>
       </div>
