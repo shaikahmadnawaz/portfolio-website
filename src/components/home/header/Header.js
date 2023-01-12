af879e825e53bb64d096e097e8618c6254e0c7fa
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-01-13 00:33:07 +0530

    in progress

--- src/components/home/header/Header.js
+++ src/components/home/header/Header.js
@@ -4,23 +4,37 @@ import Me from "../../../assets/me.svg";
 
 const Header = () => {
   return (
-    <section className="container">
-      <div className="left">
-        <h1>
-          I'm Ahmad <span>Nawaz</span>
-        </h1>
-        <p>
-          I am a Full Stack Developer
-          <br /> with experience building open source projects
-        </p>
-        <a href="#" alt="resume">
-          Download Resume
-        </a>
+    <header className="header container">
+      <div className="header-content">
+        <div className="left-header">
+          <h1 className="name">
+            Hello, <br />
+            I'm <span>Shaik Ahmad Nawaz</span>
+          </h1>
+          <p>
+            I'm a Web Developer, I love to create beautiful and functional
+            websites.
+          </p>
+          <div class="btn-con">
+            <a href={Me} download class="main-btn">
+              <span class="btn-text">Download Resume</span>
+              <span class="btn-icon">
+                <i class="fas fa-download"></i>
+              </span>
+            </a>
+          </div>
+          {/* <a href="#" alt="resume">
+            Download Resume
+          </a> */}
+        </div>
+        <div className="right-header">
+          <div class="h-shape"></div>
+          <div className="image">
+            <img src={Me} alt="Me" />
+          </div>
+        </div>
       </div>
-      <div className="right">
-        <img src={Me} alt="Me" />
-      </div>
-    </section>
+    </header>
   );
 };
 
