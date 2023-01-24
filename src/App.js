1dad48c8e2185c878ccfdd1893e4560679f59345
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-01-24 21:56:23 +0530

    Started with styled components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components";
+// import { Routes, Route } from "react-router-dom";
 
-import About from "./components/home/about/About";
-import Navbar from "./components/common/navbar/Navbar";
-import Skills from "./components/home/skills/Skills";
-import Projects from "./components/home/projects/Projects";
-import Contact from "./components/home/contact/Contact";
-import Footer from "./components/common/footer/Footer";
-import Header from "./components/home/header/Header";
+// import About from "./components/home/about/About";
+// import Navbar from "./components/common/navbar/Navbar";
+// import Skills from "./components/home/skills/Skills";
+// import Projects from "./components/home/projects/Projects";
+// import Contact from "./components/home/contact/Contact";
+// import Footer from "./components/common/footer/Footer";
+// import Header from "./components/home/header/Header";
 
 function App() {
   return (
-    <div className="main">
+    <div className="App">
       <Navbar />
+      {/* <Navbar />
       <Routes>
         <Route exact path="/" element={<Header />} />
         <Route exact path="/about" element={<About />} />
@@ -20,7 +22,7 @@ function App() {
         <Route exact path="/projects" element={<Projects />} />
         <Route exact path="/contact" element={<Contact />} />
       </Routes>
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Nav, NavbarContainer } from "./Navbar.elements";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <Nav>
+          <NavbarContainer>Nawaz</NavbarContainer>
+        </Nav>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
